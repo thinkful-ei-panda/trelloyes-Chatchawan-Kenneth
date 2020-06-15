44ffb1ad28fbc73bca26ddc100a82331eb390f1b
jenthura <joshuasth@gmail.com>
2020-06-15 15:38:29 -0500

    sooper fast

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,7 @@ import './Card.css';
 
 function Card(propsObject) {
   return (
-    <div className="Class">
+    <div className="Card">
       <button type="button">delete</button>
       <h3>{propsObject.title}</h3>
       <p>{propsObject.content}</p>
